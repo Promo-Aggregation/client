10221936fce87b1187d23e1e41eb1c2e516c5226
Ricky <audrickong.ao@gmail.com>
2019-11-14 22:33:21 +0700

    Add PUSH NOTIF & Tab Bar

---
+++ screens/home.js
@@ -0,0 +1,13 @@
+// MODULE IMPORTS
+import React from 'react';
+import { View, Text } from 'react-native';
+//FILE IMPORTS
+import styles from "../styles"
+
+export default Home = () => {
+    return (
+        <View style={styles.container}>
+            <Text>Home</Text>
+        </View>
+    );
+}
