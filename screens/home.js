c958eb619eb163602778ec4f0bdcaefc5d2802e6
Ricky <audrickong.ao@gmail.com>
2019-11-15 22:00:45 +0700

    styling

--- screens/home.js
+++ screens/home.js
@@ -1,11 +1,14 @@
 // MODULE IMPORTS
-import React, { useState } from 'react';
-import { View, Button } from 'react-native';
-import Chip from "../components/chip"
+import React, { useState, useEffect } from 'react';
+import { View, Button, FlatList } from 'react-native';
+import Axios from "axios";
 //FILE IMPORTS
+import Card from "../components/card";
+import Chip from "../components/chip";
 
 export default Home = ({ navigation }) => {
     const [tags, setTags] = useState([])
+    const [promos, setPromos] = useState([])
     const toggle = (newTag) => {
         if (tags.indexOf(newTag) === -1) {
             setTags([...tags, newTag])
@@ -17,6 +20,17 @@ export default Home = ({ navigation }) => {
     const filter = () => {
         console.log(tags)
     }
+    const fetchAll = async () => {
+        const { data: foods } = await Axios.get("http://localhost:3000/promos/dana-food")
+        setPromos(foods)
+        const { data: games } = await Axios.get("http://localhost:3000/promos/dana-game")
+        setPromos([...promos, ...games])
+        const { data: entertainment } = await Axios.get("http://localhost:3000/promos/dana-entertainment")
+        setPromos([...promos, ...entertainment])
+    }
+    useEffect(() => {
+        fetchAll()
+    }, [])
     return (
         <>
             <View
@@ -32,13 +46,14 @@ export default Home = ({ navigation }) => {
                     icon="food"
                     toggle={toggle}
                 />
-                <Chip title="Entertainment"
+                <Chip
+                    title="Entertainment"
                     icon={require("../assets/entertainment.png")}
                     toggle={toggle}
                 />
                 <Chip
-                    title="Travel"
-                    icon="beach"
+                    title="Gaming"
+                    icon="gamepad-variant"
                     toggle={toggle}
                 />
             </View>
@@ -54,10 +69,20 @@ export default Home = ({ navigation }) => {
                     style={{ position: "relative", marginLeft: 250 }}
                 />
             </View>
-            <Button
-                title="Click"
-                onPress={() => navigation.navigate("HomeDetail", { url: "https://dana.id/promo/detail/257/full" })}
-                style={[styles.container, { margin: 20 }]}
+            <FlatList
+                data={promos}
+                keyExtractor={(item, index) => String(index)}
+                renderItem={({ item: { title, date, kodePromo, detailUrl, imageUrl } }) => (
+                    <Card
+                        title={title}
+                        date={date}
+                        code={kodePromo || "No Promo Code"}
+                        detailUrl={detailUrl}
+                        imageUrl={imageUrl}
+                        navigation={navigation}
+                        type="home"
+                    />
+                )}
             />
         </>
     );
